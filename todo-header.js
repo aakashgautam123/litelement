6ed1bc1dfc13e51414a9e81e42bdf3954ff52936
Krishuthapa <thapakrishu1997@gmail.com>
2021-03-15 16:38:14 +0545

    Test branch commit.

--- todo-header.js
+++ todo-header.js
@@ -6,14 +6,28 @@ class TodoHeader extends LitElement{
    */
   static get styles() {
     return css`
-      div { height: 70px; width: 100%; background: black; color:  white; text-align: center; font-weight: bold; line-height:70px; font-size: 24px; }
+      div { 
+        height: 70px; 
+        width: 100%; 
+        background: black; 
+        color:  white; 
+        text-align: center; 
+        font-weight: bold; 
+        line-height:70px; 
+        font-size: 24px; }
     `;
   }
+
+  /**
+   * Constructor for the component.
+   */
   constructor(){
     super();
   }
+
   /**
-   * renders a template for the header 
+   * life cycle fucntion to render the elements in the component. 
+   * 
    * @returns {html}
    */
   render(){
